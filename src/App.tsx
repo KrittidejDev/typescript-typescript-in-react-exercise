bab9f7735c7cd06def68bf75d8ca6ffdeb3db412
KrittidejDev <krittidej.dev@gmail.com>
2025-08-25 18:30:59 +0700

    assignment ex 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+
+  return <Greeting name={"top"} age={20} />;
 }
 
 export default App;
